c7460f8cef1e2fa1ade36c0606171f457b73fd50
AKOTEGNON Anne-Marie <akotegnonannemarie@gmail.com>
2023-08-01 15:47:27 -0700

    repat_token_2&3

---
+++ 0x06-regular_expressions/4-repetition_token_3.rb
@@ -0,0 +1,14 @@
+#!/usr/bin/env ruby
+#above cases
+
+def find_token(sentences)
+  mot = sentences.match(/hbt*n/)
+  if mot
+    puts mot[0]
+  else
+    puts ""
+  end
+end
+
+sentences = ARGV[0]
+find_token(sentences)
